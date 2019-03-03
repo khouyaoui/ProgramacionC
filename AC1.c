1420b937cb713a5a9e8a68ee1c0078c920941fc4
Moha_ <ahrir@live.com>
2019-03-03 15:15:05 +0100

    User interface fx

--- AC1.c
+++ AC1.c
@@ -1,10 +1,46 @@
+//libs
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
+//Macros
 #define FILENAME "data/config.txt"
+#define dirName "data"
 #define MAXC 	 50
 
+typedef struct {	// aun sin usar
+	char 	filesystem_name_format[MAXC];
+	int 	num_filesystems;
+	char	first_filesystem;
+} dataConfig;
 
+//Funciones
+int listDir(){	//funcion para listarle los files a u
+    struct dirent *de;
+    DIR *dr = opendir(dirName); 
+    if (dr == NULL) { 
+        printf("No hemos podido abrir la carpeta indicada" ); 
+        return 0; 
+    }
+    printf("------------------------------------------------\n");
+    printf("Que filesystem desea seleccionar: \n");   
+    while ((de = readdir(dr)) != NULL){ 
+            if (!strcmp(de->d_name,"config.txt") == 0)
+				printf("%s\n", de->d_name);
+		}
+	printf("------------------------------------------------\n"); 
+    closedir(dr);     
+    return 0; 
+} 
+/*
+void show_filesystems(dataConfig d) {
+
+	printf("FileSytems %c\n", d.filesystem_name_format);
+
+	for (int i = 0; i < d.length; i++) {
+
+	}
+}*/
 int read_config(int *num_filesystems, char filesystem_name[], char first_filesystem[]) {
 	
 	FILE *f = fopen(FILENAME, "r");
@@ -28,14 +64,12 @@ int read_config(int *num_filesystems, char filesystem_name[], char first_filesys
 
 	}
 		fclose(f);
-	
-
-	return 1;
+	return 0;
 }
 
-
+//Funcion main
 int main(void){
-
+	listDir();
 	int num_filesystems = 0;
 	char filesystem_name[MAXC];
 	char tmp_first_filesystem[MAXC];
